d622839c38ac86d6761ccf3712e27736d6de82a1
Jagadeesh745 <157501965+Jagadeesh745@users.noreply.github.com>
2025-05-02 22:57:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>DevConnector</h1>
+      <p>A social network for developers</p>
+    </div>
+  );
+}
+export default App;
